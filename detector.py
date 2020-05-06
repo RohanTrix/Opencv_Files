9ae5c7ae85e59c8d0187cd9a77740c302863e392
RohanTrix <rohanyeahyeah@gmail.com>
2020-05-06 15:45:12 +0530

    Inital commit of Road lanes detector

---
+++ detector.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+image = cv2.imread('roads.png')
+image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
+height,width = image.shape[:2]
+
+region_of_interest_vertices = [
+    (0,height),
+    (width/2,height/2),
+    (width,height)
+    ]
+def region_of_interest(img,vertices):
+    mask = np.zeros_like(img)
+    #channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask,vertices,match_mask_color)
+    masked_image = cv2.bitwise_and(img,mask)
+    return masked_image
+
+def draw_the_lines(img,lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0],img.shape[1],3),np.uint8)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(blank_image,(x1,y1),(x2,y2),(0,255,0),thickness = 3)
+    img = cv2.addWeighted(img, 0.8,blank_image,0.2,0.0)
+    return img
+
+gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+canny_image = cv2.Canny(gray_image,100,200)
+
+
+
+cropped_image = region_of_interest(canny_image,
+                                   np.array([region_of_interest_vertices],
+                                            np.int32))
+
+lines = cv2.HoughLinesP(cropped_image,rho = 6,theta = np.pi/60,threshold = 160
+                        ,lines = np.array([]),minLineLength = 40,
+                        maxLineGap = 25)
+
+
+image_with_lines = draw_the_lines(image,lines)
+plt.imshow(image)
+plt.imshow(cropped_image)
+plt.imshow(image_with_lines)
+
+plt.show()
+
+
+
+
