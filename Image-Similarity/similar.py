82fc973ae4a42d41ed31b0ed3b77578ea91de68b
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-22 23:42:54 +0530

    Committed Changes

--- Image-Similarity/similar.py
+++ Image-Similarity/similar.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
 org = cv2.imread('main_img.png')
+duplicate = cv2.imread('test1.png')
 
-cv2.imshow('MAIN', org)
+cv2.imshow('Main',org)
+cv2.imshow('test',duplicate)
+same_size_and_channels = False
+if org.shape == duplicate.shape:
+    diff = cv2.subtract(duplicate, org)
+    cv2.imshow('Subtract', diff)
+    b, g, r = cv2.split()
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
