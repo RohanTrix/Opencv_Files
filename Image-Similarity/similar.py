6290e7d530a3d44aa1b5198769aed02fc89fe15c
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-22 23:09:27 +0530

    Committed changes

--- Image-Similarity/similar.py
+++ Image-Similarity/similar.py
@@ -1,34 +1,6 @@
-import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-from tkinter.filedialog import askopenfilename
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 23:06:13 2020
 
-filename1 = "test1.png"
-filename2 = "test2.png"
-
-img1=cv2.imread(filename1,4)
-img2=cv2.imread(filename2,4)
-
-# Initiate SURF detector
-surf=cv2.xfeatures2d.SURF_create()
-
-# find the keypoints and descriptors with SURF
-kp1, des1 = surf.detectAndCompute(img1,None)
-kp2, des2 = surf.detectAndCompute(img2,None)
-
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
-
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-        a=len(good)
-        percent=(a*100)/len(kp2)
-        print("{} % similarity".format(percent))
-        if percent >= 75.00:
-            print('Match Found')
-        if percent < 75.00:
-            print('Match not Found')
+@author: ROHAN
+"""
