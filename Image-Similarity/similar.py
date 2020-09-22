1bf5bd2c0fc4adc25b9fcce363893aa78914eb0a
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-22 23:54:47 +0530

    Committed Changes

--- Image-Similarity/similar.py
+++ Image-Similarity/similar.py
@@ -6,12 +6,12 @@ duplicate = cv2.imread('test1.png')
 
 cv2.imshow('Main',org)
 cv2.imshow('test',duplicate)
-same_size_and_channels = False
+same_image = False
 if org.shape == duplicate.shape:
     diff = cv2.subtract(duplicate, org)
     cv2.imshow('Subtract', diff)
-    b, g, r = cv2.split()
-
-
+    b, g, r = cv2.split(diff)
+    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g)==0 and cv2.countNonZero(r):
+        same_image = True
 cv2.waitKey(0)
 cv2.destroyAllWindows()
