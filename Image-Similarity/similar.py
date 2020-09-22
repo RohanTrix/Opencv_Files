8f4c4acb6096e7e8ecd55265602f6ef93f68b126
RohanTrix <rohanyeahyeah@gmail.com>
2020-09-23 00:54:53 +0530

    Committed Changes

--- Image-Similarity/similar.py
+++ Image-Similarity/similar.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 org = cv2.imread('main_img.png')
-img_to_compare = cv2.imread('test2.png')
+img_to_compare = cv2.imread('test1.png')
 
-#######################
-plt.imshow(org)
-#######################
+
 org = org[88:682, 508: 843]
 img_to_compare = img_to_compare[88:682, 508: 843]
 
 cv2.imshow('Main',org)
 cv2.imshow('test',img_to_compare)
+
+
 same_image = False
 if org.shape == img_to_compare.shape:
     diff = cv2.subtract(img_to_compare, org)
@@ -19,6 +19,7 @@ if org.shape == img_to_compare.shape:
     if cv2.countNonZero(b) == 0 and cv2.countNonZero(g)==0 and cv2.countNonZero(r):
         same_image = True
     if same_image:
+        print('same image')
         quit()
 
 sift = cv2.xfeatures2d.SIFT_create()
